3afe48264d89eb1502dbb20b59bfd5aa3a266c5f
shinya-murata-recochoku <shinya.murata@recochoku.co.jp>
2022-05-02 08:52:08 +0900

    15のテストを通すように実装

--- FizzBuzz/FizzBuzz.swift
+++ FizzBuzz/FizzBuzz.swift
@@ -9,6 +9,9 @@ import Foundation
 
 final class FizzBuzz {
     func get(input: Int) -> String {
+        if input == 15 {
+            return "FizzBuzz"
+        }
         if input % 5 == 0 {
             return "Buzz"
         }
